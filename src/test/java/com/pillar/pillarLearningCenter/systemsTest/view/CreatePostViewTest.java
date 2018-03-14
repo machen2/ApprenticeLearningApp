4777fe99f9403393b690f0bfd0843ad353852b3c
Tori Machen <tmachen@pillartechnology.com>
2018-03-14 16:11:46 -0500

    Tested end point works

---
+++ src/test/java/com/pillar/pillarLearningCenter/systemsTest/view/CreatePostViewTest.java
@@ -0,0 +1,30 @@
+package com.pillar.pillarLearningCenter.systemsTest.view;
+
+import com.pillar.pillarLearningCenter.controller.PostController;
+import com.pillar.pillarLearningCenter.service.PostService;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(PostController.class)
+public class CreatePostViewTest {
+    @Autowired
+    MockMvc mockMvc;
+
+    @MockBean
+    PostService postServiceMock;
+
+    @Test
+    public void testPostNewRouteLoadsSuccessfully() throws Exception {
+        mockMvc.perform(get("/posts/new"))
+                .andExpect(status().isOk());
+    }
+}
